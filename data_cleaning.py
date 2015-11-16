3ec8ab812d5bb0406c6f3e0ae6291fbad21466e5
Berkeley Almand-Hunter <berkeleyalmandhunter@gmail.com>
2015-11-16 00:31:18 -0800

    added visualizations and logistic regression

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from scipy import stats
+import numpy as np
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.linear_model import LogisticRegression
 
 
 def find_columns_containing_nan(df, col_names):
@@ -36,7 +40,7 @@ def replace_missing_values(df, fill_method):
     elif fill_method == 6:
         df.interpolate(inplace=True)
     return df
-    
+
 
 def convert_column_to_float(df, column):
     for idx, row in enumerate(df[column]):
@@ -56,3 +60,35 @@ def locate_non_numeric_values(df, col_name):
             non_num_dict[idx] = row['five']
     print non_num_dict
     return non_num_dict
+
+
+def make_dummy_variables(df, columns_to_encode):
+    df_dummies = pd.get_dummies(df[columns_to_encode])
+    print df_dummies
+    df = pd.concat([df, df_dummies], axis=1)
+    df.drop(columns_to_encode, axis=1, inplace=True)
+    return df
+
+
+def make_poly_feat(df, features):
+    transformer_log = FunctionTransformer(np.log1p)
+    transformer_sq = FunctionTransformer(np.square)
+    transformer_sqrt = FunctionTransformer(np.sqrt)
+    for column in features:
+        name = 'log_' + str(column)
+        df[name] = np.transpose(transformer_log.transform(df[column]))
+        name_sq = 'sq_' + str(column)
+        df[name_sq] = np.transpose(transformer_sq.transform(df[column]))
+        name_sqrt = 'sqrt_' + str(column)
+        df[name_sqrt] = np.transpose(transformer_sqrt.transform(df[column]))
+    return df
+
+
+def create_interaction_feat(df, features):
+    for idx, col in enumerate(features):
+        i = idx + 1
+        while i < 8:
+            name = str(col) + '_inter_' + str(features[i])
+            df[name] = df[col]*df[features[i]]
+            i += 1
+    return df
--- plots.py
+++ plots.py
@@ -2,11 +2,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+def plot_params():
+    size = 22
+    a = plt.rc('xtick', labelsize = size)
+    b = plt.rc('ytick', labelsize = size)
+    return a, b, plt.gca(), size
+
+
 def plot_columns(df, col_names):
+    plt.figure(facecolor='w', figsize = (5,5))
+    a, b, axes, label_size = plot_params()
     for column in col_names:
         try:
             plt.title(column)
             df[column].plot()
-            plt.figure(figsize = (10,5))
+            plt.figure(figsize = (5,5))
         except:
-            print "Cannon plot column", column
+            print "Cannot plot: ", column
+
+
+def plot_column_hist(df, col_names):
+    plt.figure(facecolor='w', figsize = (5,5))
+    a, b, axes, label_size = plot_params()
+    for column in col_names:
+        try:
+            plt.title(column)
+            df[column].hist()
+            plt.figure(figsize = (5,5))
+        except:
+            print "Cannot plot: ", column
